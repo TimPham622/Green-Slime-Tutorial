6c9788bc3da2d450d3b0f32bf1389d1aad77ac37
TimPham622 <a1944756@adelaide.edu.au>
2025-07-31 21:49:34 +0930

    realised i had to read the task sheet

--- main.cpp
+++ main.cpp
@@ -1,13 +1,30 @@
-#include "Referee.h"
+#include <bits/stdc++.h>
+using namespace std;
 #include "Player.h"
 #include "Human.h"
 #include "Computer.h"
-#include <bits/stdc++.h>
+#include "Referee.h"
 
-int main(){
-    Computer comp;
-    Human hum;
-    Referee ref;
+int main() {
+    cout << "Enter your name: ";
+    string playerName;
+    getline(cin, playerName);
 
-    Player* finish = ref.refGame(&hum,&comp);
+    if(playerName.empty()) {
+        playerName = "Human";
+    }
+
+    Human human(playerName);
+    Computer computer;
+
+    Referee referee;
+    Player* winner = referee.refGame(&human, &computer);
+
+    if(winner == nullptr) {
+        cout << "It's a Tie.";
+    } else {
+        cout << winner->getName() << " Wins.";
+    }
+
+    return 0;
 }
