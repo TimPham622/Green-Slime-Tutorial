74d4ffc07cabf0210520446f7b3c6ae6778aa4ae
Tim <a1944756@adelaide.edu.au>
2025-08-10 19:34:51 +0930

    Adding new LOCs and actually reading the article

--- Human.cpp
+++ Human.cpp
@@ -1,16 +1,13 @@
 #include "Human.h"
 #include "MoveSelector.h"
 #include <iostream>
-#include <ostream>
+#include <ostream> 
 
 Human::Human(std::string name) : Player(name) {}
 
 Move* Human::makeMove() {
     std::string input;
-
     std::cout << "Enter Move: " << std::flush; 
-    
     std::cin >> input;
-
     return MoveSelector::createMove(input);
 }
