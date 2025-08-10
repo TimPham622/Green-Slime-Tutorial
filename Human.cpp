696ee7a0e377a63b5b8f59af722ac0cc47be7118
Tim <a1944756@adelaide.edu.au>
2025-08-10 19:22:56 +0930

    Forgot to add an extra LOC

--- Human.cpp
+++ Human.cpp
@@ -1,13 +1,15 @@
 #include "Human.h"
 #include "MoveSelector.h"
 #include <iostream>
-
+#include <ostream> 
 
 Human::Human(std::string name) : Player(name) {}
 
 Move* Human::makeMove() {
     std::string input;
-    std::cout << "Enter Move: ";
+
+    std::cout << "Enter Move: " << std::flush; 
+    
     std::cin >> input;
 
     return MoveSelector::createMove(input);
