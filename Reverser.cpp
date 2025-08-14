84402ab053006414bccfe0faae557b9da0063483
TimPham622 <a1944756@adelaide.edu.au>
2025-08-14 16:48:57 +0930

    Added the new files

---
+++ Reverser.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+class Reverser{
+
+public:
+int reverseDigit(int value) {
+    std::cout << value % 10;
+    if (value < 10) {
+        return -1;
+    }
+    return reverseDigit(value / 10);
+}
+
+std::string reverseString(std::string characters) {
+    if (characters.length() <= 0) {
+        return characters;
+    }
+    else {
+        return reverseString(characters.substr(1) + characters[0]);
+    }
+}
+
+};
