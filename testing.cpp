e154927528cdb32dcdde3d8b864a0a88649e6d9f
TimPham622 <a1944756@adelaide.edu.au>
2025-08-22 07:34:40 +0930

    Added workshop code

--- testing.cpp
+++ testing.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
-int help(int n, int i, int prev, int curr, std::vector<int>& result) {
 
+// You are given a number n which can be and eg. 5. generate a pattern that will print 1,2,3,4,5,4,3,2,1
+//Generate this recursively
+
+
+void print(int current, int n) {
+    if (current > n) {
+        return;
+    }
+
+    std::cout << current << " ";
+    print(current + 1, n);
+
+    if (current != n) { 
+        std::cout << current << " ";
+    }
 }
 
-int modifiedFibonacci(int n) {
-    
+int main() {
+    int n =5;
+    print(1, n);
+    return 0;
 }
